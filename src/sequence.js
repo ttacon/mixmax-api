721f6fc7560c387e1e8159034cf27e9b5a0fa59d
Trey Tacon <ttacon@gmail.com>
2020-10-03 17:26:50 -0400

    feat: add pause, resume and get for sequences

--- src/sequence.js
+++ src/sequence.js
@@ -49,9 +49,36 @@ class SequenceHandler {
     return result.body.recipients;
   }
 
+  async pause() {
+    const result = await this._client.post(`sequences/${this._sequenceID}/pause`);
+
+    if (result.statusCode !== 200) {
+      throw new Error('failed to pause Sequence, status: ' + result.statusCode);
+    }
+  }
+
+  async resume() {
+    const result = await this._client.post(`sequences/${this._sequenceID}/resume`);
+
+    if (result.statusCode !== 200) {
+      throw new Error('failed to resume Sequence, status: ' + result.statusCode);
+    }
+  }
+
   get recipients() {
     return new RecipientsHandler(this._client, this._sequenceID);
   }
+
+  async get() {
+    const result = await this._client.get(`sequences/${this._sequenceID}`);
+
+    if (result.statusCode !== 200) {
+      throw new Error('failed to resume Sequence, status: ' + result.statusCode);
+    }
+    return result.body;
+  }
+
+  
 
 }
 
